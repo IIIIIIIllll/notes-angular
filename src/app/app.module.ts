0796f18a426cb5c625b81c3479c3afe8c14c619f
allin17 <ivanovzahar1000@gmail.com>
2023-07-28 17:55:13 +0300

    create components and create notes header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    NoteDetailComponent,
+    NotesListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-notes.ts
@@ -0,0 +1,11 @@
+import { Note } from './models/note';
+
+export const NOTES: Note[] = [
+    { title: 'BMW', text: 'Luxury and performance in one package.' },
+    { title: 'Toyota', text: 'Reliable and fuel-efficient vehicles.' },
+    { title: 'Ford', text: 'Iconic American carmaker with a wide range of models.' },
+    { title: 'Mercedes-Benz', text: 'Exemplary engineering and opulent comfort.' },
+    { title: 'Audi', text: 'Combining elegance, technology, and performance.' },
+    { title: 'Volkswagen', text: 'Well-built cars with a touch of European flair.' },
+  ];
+  
