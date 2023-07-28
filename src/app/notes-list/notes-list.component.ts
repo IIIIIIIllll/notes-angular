7d785b88083aac1e7b64c299200829d68a55fae2
allin17 <ivanovzahar1000@gmail.com>
2023-07-28 19:09:39 +0300

    create notes-list

--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../note.service';
+import { Note } from '../models/note';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.css']
 })
-export class NotesListComponent {
+export class NotesListComponent implements OnInit {
 
+  selectedNote?: Note;
+
+  notes: Note[] = [];
+
+  constructor(private noteService: NoteService) {}
+
+  ngOnInit(): void {
+    this.getNotes();
+  }
+
+  onSelect(note: Note): void {
+    this.selectedNote = note;
+    this.noteService.setSelectedNote(note);
+  }
+
+  getNotes() {
+    this.noteService.getNotes()
+      .subscribe(notes => this.notes = notes)
+  }
 }
