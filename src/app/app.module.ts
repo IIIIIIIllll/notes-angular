8dc6ac2b9c731af1c234c73f9c7c2770e19c3a35
allin17 <ivanovzahar1000@gmail.com>
2023-07-29 13:36:55 +0300

    add add-note modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddNoteModalComponent } from './modals/add-note-modal/add-note-modal.component';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
     NoteDetailComponent,
-    NotesListComponent
+    NotesListComponent,
+    AddNoteModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modals/add-note-modal/add-note-modal.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Note } from 'src/app/models/note';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-note-modal',
+  templateUrl: './add-note-modal.component.html',
+  styleUrls: ['./add-note-modal.component.css']
+})
+export class AddNoteModalComponent {
+  title: string = '';
+  text: string = '';
+
+  constructor(public dialogRef: MatDialogRef<AddNoteModalComponent>) {}
+
+  onAddNote(): void {
+    const newNote: Note = {
+      title: this.title,
+      text: this.text,
+    };
+    this.dialogRef.close(newNote);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+}
